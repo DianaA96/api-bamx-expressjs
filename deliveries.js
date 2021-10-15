9e1680285249414a6d6103d997d676374f521482
Heron Samperio <joseheronsamperio@gmail.com>
2021-10-15 11:15:56 -0500

    Creacion de endpoint Modificacion Donativo Receptor

--- deliveries.js
+++ deliveries.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router()
 const {QueryTypes} = require('sequelize');
-const {Delivery, Warehouse} = require('./database');
+const {Delivery, Categorie, DeliveredQuantity} = require('./database');
 
 const {DB}  = require('./database')
 
@@ -37,5 +37,51 @@ router.get('/:idReceiver', async (req, res, next) => {
 )
 
 //AGREGAR POST DE MODIFICACION DONATIVO RECEPTOR :()
+router.post('/', async (req, res, next)=>{
+    const {delivery} = req.body;
+    const {delivered} = req.body;
+    console.log(delivered.ent1.cantidad)
+    try{
+        await Delivery.create(delivery)
+        .then(async (a)=>{
+            await DeliveredQuantity.create(
+                {idDelivery: a.idDelivery,
+                idCategory: delivered.ent1.idCategoria,
+                cantidad: delivered.ent1.cantidad}
+                )
+            if(delivered.ent2){
+                await DeliveredQuantity.create(
+                    {idDelivery: a.idDelivery,
+                    idCategory: delivered.ent2.idCategoria,
+                    cantidad: delivered.ent2.cantidad}
+                    )
+            }
+            if(delivered.ent3){
+                await DeliveredQuantity.create(
+                    {idDelivery: a.idDelivery,
+                    idCategory: delivered.ent3.idCategoria,
+                    cantidad: delivered.ent3.cantidad}
+                    )
+            }
+            if(delivered.ent4){
+                await DeliveredQuantity.create(
+                    {idDelivery: a.idDelivery,
+                    idCategory: delivered.ent4.idCategoria,
+                    cantidad: delivered.ent4.cantidad}
+                    )
+            }
+            return res.status(200).json({
+                entrega: delivery,
+                entregado1: delivered.ent1,
+                entregado2: delivered.ent2,
+                entregado3: delivered.ent3,
+                entregado4: delivered.ent4
+            })
+        })
+    }catch(err) {
+        next(err)
+    }
+})
+
 
 module.exports = router
